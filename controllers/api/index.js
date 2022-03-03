4b38ff9b607ea478efa519ed6bd5fe10dc948e7c
michael Means <colemeans51@gmail.com>
2022-03-03 14:33:20 -0600

    created addpost.js and linkined to edit.handlebars

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,11 +1,20 @@
+const postRoutes = require('./post-routes');
+
 const router = require('express').Router();
 
-const userRoutes = require('./user-routes.js');
-const postRoutes = require('./post-routes');
 const commentRoutes = require('./comment-routes');
 
+const userRoutes = require('./user-routes.js');
+
+
+
 router.use('/users', userRoutes);
+
 router.use('/posts', postRoutes);
+
 router.use('/comments', commentRoutes);
+
+
+
 
 module.exports = router;
